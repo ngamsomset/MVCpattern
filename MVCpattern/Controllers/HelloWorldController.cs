a1ecc0207c90f7d64fa4d3c6288174bb8fd3e39b
brian <brian@DESKTOP-CK1BIND>
2023-11-04 20:12:13 +1100

    Add project files.

---
+++ MVCpattern/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCpattern.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public String Index()
+        {
+            return "This is the index page";
+        }
+
+        public String Hello()
+        {
+            return "Who's there";
+        }
+    }
+}
---
+++ MVCpattern/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCpattern.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
